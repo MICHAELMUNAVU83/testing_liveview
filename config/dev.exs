73d2906cea4c379c3f7b9baad80d5316871b387d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-11-30 08:05:14 +0300

    Tested home

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :testing_liveview, TestingLiveview.Repo,
 config :testing_liveview, TestingLiveviewWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4200],
+  http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
